52a8779d53c170e6d6ae7cd98e85ce543ca6c747
Jose de Paula E. Junior <jose.junior@gmail.com>
2009-02-06 20:54:45 -0200

    Initial import

---
+++ src/model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8 
+
+import datetime
+from elixir import *
+
+class Basic(Entity):
+    
+    using_options(autosetup = True)
+    
+    name = Field(Unicode(64))
+    location = Field(Unicode(64))
+    initial_date = Field(DateTime, default = datetime.datetime.now)
+    scope = Field(UnicodeText)
+    
+class Asset(Entity):
+    
+    name = Field(Unicode(64))
+    description = Field(UnicodeText)
+    value = Field(Numeric)
+    
+    vulns = ManyToMany('Vulnerabilities')
+    
+class Vulnerabilities(Entity):
+    
+    description = Field(Unicode(256))
+    severity = Field(Numeric)
+    chance = Field(Numeric)
+    
+    assets = ManyToMany('Asset')
+    threats = ManyToMany('Threat')
+    
+class Threat(Entity):
+    
+    name = Field(Unicode(64))
+    description = Field(Unicode(256))
+    
+    vulns = ManyToMany('Vulnerabilities')
